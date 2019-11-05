4266477f5c392ebdb398253f88ba17d65bff2140
putrayusifin <52587456+putrayusifin@users.noreply.github.com>
2019-11-05 10:01:02 +0700

    version1
    ffr

---
+++ main.java
@@ -0,0 +1,10 @@
+package pegadaianGIT;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("this is version 1");
+		System.out.println("tes");
+		System.out.println("biar familiar");
+	}
+
+}
